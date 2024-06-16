61d7777710126ee8df69d1ee83f7bb14aaeed3ea
tkg111 <67671437+tkg111@users.noreply.github.com>
2024-06-16 18:02:55 +0800

    added movement

--- tkgcs/Assets/mouselook.cs
+++ tkgcs/Assets/mouselook.cs
@@ -13,7 +13,7 @@ public class mouselook : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         
 
     }
